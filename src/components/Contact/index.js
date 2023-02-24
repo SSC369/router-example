8c4a2543a739907d77c11f432c6d2e5a43d35d71
SSC369 <sai9821c@gmail.com>
2023-02-24 18:12:55 +0000

    routing

---
+++ src/components/Contact/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
+      alt="contact"
+      className="home"
+    />
+    <h1 className="home-heading">Contact</h1>
+  </div>
+)
+
+export default Home
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      alt="not found"
+      className="home"
+    />
+    <h1 className="home-heading">Not Found</h1>
+  </div>
+)
+
+export default Home
